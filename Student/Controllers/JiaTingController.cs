e5d6f22e0f9ff44badee68e187b140a4d02a1edc
李甲森 <2730166172@qq.com>
2019-04-15 11:37:08 +0800

    4/15  呵呵

---
+++ Student/Controllers/JiaTingController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Mo.Models;
+using Microsoft.AspNetCore.Cors;
+
+namespace Student.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/JiaTing/[action]")]
+    [EnableCors("Kua")]
+    public class JiaTingController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        [HttpGet]
+        public List<Mo.Models.JiaTingZhuang> GetJia()
+        {
+            return db.JiaTingZhuang.ToList();
+        }
+
+
+
+    }
+}
---
+++ Student/Controllers/StuXinController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using Mo.Models;
+
+namespace Student.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/StuXin/[action]")]
+    [EnableCors("Kua")]
+    public class StuXinController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        [HttpGet]
+        public List<Mo.Models.Student> GetStus()
+        {
+            return db.Student.ToList();
+        }
+
+        [HttpPost]
+        public string Create([FromBody]Mo.Models.Student info)
+        {
+            try
+            {
+                db.Entry(info).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+                db.SaveChanges();
+            }
+            catch(Exception ex)
+            {
+                return ex.ToString();
+            }
+            return "添加成功！";
+        }
+
+    }
+}
